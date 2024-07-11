24990901036318c1da18c8e996cbfad371f19c88
Narayan <narayanbhandari556@gmail.com>
2024-07-11 12:30:04 +0530

    vite and react

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Code from "./code";
+function App() {
+  return (
+    <>
+      <Code />
+      <h1>code aur react js</h1>
+      <p>Lorem ipsum dolor sit?</p>
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/code.jsx
@@ -0,0 +1,5 @@
+function Code() {
+  return <h3>code is ready</h3>;
+}
+
+export default Code;
