f1375d2a1b32482f847243eddbf176a1bfb122dd
Narayan <narayanbhandari556@gmail.com>
2024-07-12 11:08:11 +0530

    custom react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,10 @@
 import Code from "./code";
 function App() {
+  const username = "chai aur code";
   return (
     <>
       <Code />
-      <h1>code aur react js</h1>
+      <h1>code aur react js{username}</h1>
       <p>Lorem ipsum dolor sit?</p>
     </>
   );
